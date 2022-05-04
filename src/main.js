97db395004f91290d5a0a283ef63be5b31d19275
QQKdeGit <627948700@qq.com>
2022-05-04 21:53:16 +0800

    build: remove element plus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import 'element-plus/theme-chalk/index.css';
 import App from './App.vue';
 
 const app = createApp(App)
-app.use(ElementPlus)
 app.mount('#app')
